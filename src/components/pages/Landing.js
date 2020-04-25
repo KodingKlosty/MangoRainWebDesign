03ef54e9e70743eb5e20f548589687b30eee9fba
James Klostermeyer Jr <42751399+KodingKlosty@users.noreply.github.com>
2020-04-25 12:31:45 -0400

    Landing Page Complete

--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -1,18 +1,20 @@
-// needed components
-import React, {Component} from 'react'
-
 // styles import
 import  '../../styles/landing.scss'
+
+// needed components
+import React, {Component} from 'react'
+import { Container,Row,Col } from 'reactstrap'
 
 class Landing extends Component {
     render (){
         return(
-            <div>
-                <div className='main'>
-                    <h1>Web Designer for Hire. 
-                    <img  src="https://via.placeholder.com/500x500?text=Picture+Selction+Needed" alt="Web Designer"/>
-                    </h1>
-                </div>
+            <div className='main'>
+                <Container className='mainContainer'>
+                    <Row  lg='2'>
+                        <Col className='eightyPx'>Hire me to build your next website</Col>
+                        <Col><img  src="https://via.placeholder.com/500x500?text=Picture+Selction+Needed" alt="Web Designer"/> </Col>
+                    </Row>
+                </Container>
             </div>
         )  // End Return
     } // End Render
