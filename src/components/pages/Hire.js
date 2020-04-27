ce24d31c781960b85f5629d977e39d0ebe7e412e
James Klostermeyer Jr <42751399+KodingKlosty@users.noreply.github.com>
2020-04-27 17:15:14 -0400

    Client Page completed
    ClientPortal is complete.
     Starting Hire Page.

--- src/components/pages/Hire.js
+++ src/components/pages/Hire.js
@@ -1,10 +1,37 @@
 import React, {Component} from 'react'
+import { Button, 
+    Form, 
+    FormGroup, 
+    FormText,
+    Label, 
+    Input, 
+    Container} from 'reactstrap'
 
 class Hire extends Component {
     render (){
         return(
             <div className='main'>
-                Hire Page working
+               <Container>
+                   <Form className='hireForm'>
+                       <FormGroup>
+                           <FormText>
+                                If you would like to contact me please fill out the form below.
+                           </FormText>
+                       </FormGroup>
+                       <FormGroup>
+                           <Label>Company Name:</Label>
+                           <Input />
+                       </FormGroup>
+                       <FormGroup>
+                           <Label>Name:</Label>
+                           <Input />
+                       </FormGroup>
+                       <FormGroup>
+                           <Label>Message:</Label>
+                           <Input type='textarea' />
+                       </FormGroup>
+                   </Form>
+               </Container>
             </div>
         )  // End Return
     } // End Render
