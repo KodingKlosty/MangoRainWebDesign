80922287c58503012302c75d29e44b0426cf2ea0
James Klostermeyer Jr <42751399+KodingKlosty@users.noreply.github.com>
2020-04-27 23:31:50 -0400

    Nightly Upload 04-27-2020
    reCaptcha v2 on Hire Me page is not working properly.
    
    humanCheck is not firing when the form submission is executed

--- src/components/pages/Hire.js
+++ src/components/pages/Hire.js
@@ -1,35 +1,77 @@
+//styles
+import '../../styles/hireMe.scss'
+// components
 import React, {Component} from 'react'
 import { Button, 
     Form, 
     FormGroup, 
-    FormText,
     Label, 
     Input, 
     Container} from 'reactstrap'
+import Recaptcha from 'react-recaptcha'
 
 class Hire extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            isHuman: false
+        }
+    }
+
+
+    onloadCallback = () => {
+        console.log('captcha loaded')
+    }
+
+    verifyCallback = (res) => {
+        if(res){
+            this.setState({isHuman: true})
+        }
+        else {
+            this.setState({isHuman:false})
+        }
+    }
+
+    humanCheck = (event) => {
+        event.preventDefault()
+        console.log('humanCheck')
+        alert("Test")
+       /* if(isHuman){
+            alert('Thank You for your intrest. We will get in contact with you shortly')
+        }
+        else{
+            alert('You must be a robot. Please check the reCaptcha')
+        }*/
+
+    }
     render (){
         return(
             <div className='main'>
-               <Container>
-                   <Form className='hireForm'>
-                       <FormGroup>
-                           <FormText>
-                                If you would like to contact me please fill out the form below.
-                           </FormText>
+               <Container className='hireForm'>
+                   <Form>
+                       <FormGroup className='subHeader'>
+                            <h2>If you would like to contact me please fill out the form below and click submit.</h2>                    
                        </FormGroup>
                        <FormGroup>
-                           <Label>Company Name:</Label>
+                           <Label className='label'>Company Name:</Label>
                            <Input />
                        </FormGroup>
                        <FormGroup>
-                           <Label>Name:</Label>
+                           <Label className='label'>Name:</Label>
                            <Input />
                        </FormGroup>
                        <FormGroup>
-                           <Label>Message:</Label>
+                           <Label className='label'>What can I help you with?</Label>
                            <Input type='textarea' />
                        </FormGroup>
+                       <FormGroup>
+                           <Recaptcha 
+                           sitekey='6LesSu8UAAAAAFupkFF4jVLx1cnVQc4W41Zv_qs7'
+                           render='explicit'
+                           onloadCallback={this.onloadCallback}
+                           verifyCallback={this.verifyCallback}/>
+                       </FormGroup>
+                       <Button className="submitBtn" type='submit' onClick={() => this.humanCheck()}>Submit</Button>
                    </Form>
                </Container>
             </div>
